07d717220db32972394caa3dd4d2fce66ef63e75
Diego Casemiro <dcfs1998@gmail.com>
2024-05-12 09:33:52 -0300

    finalizando o exercicio proposto pela DIO no curso de java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+    Double saldo = 237.48;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Ola, digite o numero da sua agencia: ");
+    String agencia = scanner.nextLine();
+
+    System.out.println("Digite o numero da sua conta: ");
+    String numero = scanner.nextLine();
+
+    System.out.println("Digite seu nome: ");
+    String nome = scanner.nextLine();
+
+    System.out.print("Olá, " + nome + "! Obrigado por criar uma conta em nosso banco, ");
+    System.out.print("sua agência é " + agencia);
+    System.out.print(", a conta é " + numero);
+    System.out.print(" e seu saldo de " + saldo + " já esta disponivel para saque.");
+
     }
+    
 }
